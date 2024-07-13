fe41d5bd5e0a300c8c7dfc7b977e2fd07c23e846
annalitvin <litvin_any@ukr.net>
2024-07-13 07:29:27 +0300

    fixed according to tox

--- app/api/api.py
+++ app/api/api.py
@@ -13,6 +13,7 @@ from fastapi import status
 
 api_router = APIRouter()
 
+
 @api_router.post("/predict", response_model=PredictResponse)
 async def predict(request: Request, payload: PredictRequest) -> Any:
     """
