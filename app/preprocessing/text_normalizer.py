629658a17724a5ec06005c7abfdc1637f7e980a1
annalitvin <litvin_any@ukr.net>
2024-07-17 10:07:08 +0300

    Added comments

--- app/preprocessing/text_normalizer.py
+++ app/preprocessing/text_normalizer.py
@@ -2,44 +2,59 @@ import re
 import nltk
 
 from collections import Counter
+from typing import Text
 
 from app.preprocessing.tools import get_stopwords
 
 
 class TextNormalizer:
 
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, raw_text: Text):
+        self.text: Text = raw_text
         self.to_lowercase()
         self.delete_punctuation()
         self.delete_numbers()
 
-        self.words = self.tokenize(self.text)
+        self.words: list = self.tokenize(self.text)
 
     @staticmethod
-    def tokenize(text):
+    def tokenize(text) -> list:
         words = nltk.word_tokenize(text)
         return words
 
     def to_lowercase(self):
+        """Convert all characters to lowercase from list of tokenized words"""
         self.text = self.text.lower()
 
     def delete_punctuation(self):
+        """Delete punctuation from text"""
         self.text = re.sub(r"[^\w\s]", "", self.text)
 
     def delete_numbers(self):
+        """Delete numbers from list of tokenized words"""
         self.text = re.sub(r"\d", "", self.text)
 
-    def remove_rare_words(self, threshold=2):
+    def remove_rare_words(self, threshold: int = 2):
+        """
+        Remove rare words from list of tokenized words
+        :arg:
+            threshold: int - number of times a word appears.
+        """
         words = self.tokenize(self.text)
         word_freq = Counter(words)
         filtered_words = [word for word in self.words if word_freq[word] >= threshold]
         self.text = " ".join(filtered_words)
 
-    def delete_stopwords(self, stop_words):
+    def delete_stopwords(self, stop_words: list):
+        """Remove stop words from list of tokenized words
+        :arg:
+            stop_words: list - Stop words are the words in a stop list (or stoplist or negative dictionary)
+            which are filtered out (i.e. stopped) before or after processing of natural language data (text)
+            because they are deemed insignificant.
+        """
         self.words = [token for token in self.words if token not in stop_words]
 
-    def normalize(self, del_stopwords=False, remove_rare_words=False):
+    def normalize(self, del_stopwords: bool = False, remove_rare_words: bool = False):
         if remove_rare_words:
             self.remove_rare_words()
         if del_stopwords:
--- app/preprocessing/text_preparation.py
+++ app/preprocessing/text_preparation.py
@@ -1,8 +1,9 @@
 import nltk
 
-from typing import List
+from typing import List, Text
 from tqdm import tqdm
 
+from nltk import tree
 from nltk import WordNetLemmatizer
 from nltk import PorterStemmer
 from nltk.sentiment import SentimentIntensityAnalyzer
@@ -16,17 +17,38 @@ class TextPreparation:
         self,
         text: str,
     ):
-        self.text_normalizer = TextNormalizer(text)
+        self.text_normalizer: object = TextNormalizer(text)
 
     @property
-    def text_removed_from_rare_words(self):
+    def text_removed_from_rare_words(self) -> Text:
+        """
+        Retrieve cleared text including stop words.
+        :return:
+            Text - text with  stop words.
+        """
         filtered_tokens = self.cleaned_text(del_stopwords=False, remove_rare_words=True)
         return " ".join(filtered_tokens)
 
-    def cleaned_text(self, del_stopwords=False, remove_rare_words=False):
+    def cleaned_text(self, del_stopwords=False, remove_rare_words=False) -> List[Text]:
+        """
+        Retrieve cleared text.
+        :arg:
+            del_stopwords (Optional): - remove stop words from a text corpus.
+            remove_rare_words (Optional): remove rare words from a text corpus.
+        :return:
+            Text - cleared text.
+        """
         return self.text_normalizer.normalize(del_stopwords=del_stopwords, remove_rare_words=remove_rare_words)
 
-    def stemmed(self):
+    def stemmed(self) -> List[str]:
+        """
+        Stemming is the process of reducing the tokens to their root forms, which are not necessarily valid words.
+        For example, the words 'running', 'runs', and 'run' can be stemmed to the root form 'run'.
+        Stemming can help group together words that have similar meanings but different forms.
+        :return:
+            list - stemmed words.
+        """
+
         stemmer: PorterStemmer = PorterStemmer()
         filtered_tokens = self.cleaned_text(del_stopwords=True, remove_rare_words=True)
         stemmed = set()
@@ -41,9 +63,7 @@ class TextPreparation:
         To reduce the different forms of a word to one single form,
         for example, reducing "builds", "building",or "built" to the lemma "build":
         Compounds were lemmatized, that is, inflectional differences were disregarded.
-        Args:
-            words: numpy array - words that are obtained from the dataset.
-        returns:
+        :return:
             list - lemmatized words.
         """
         lemmatizer: WordNetLemmatizer = WordNetLemmatizer()
@@ -55,13 +75,36 @@ class TextPreparation:
         lemmatized_tokens = list(lemmatized_words)
         return lemmatized_tokens
 
-    def extract_parts_of_speech(self):
+    def extract_parts_of_speech(self) -> tree.Tree:
+        """
+        Part-of-speech tagging is the process of assigning a grammatical category to each
+        token, such as noun, verb, adjective, etc.
+        Part-of-speech tagging can help us understand the structure and meaning of the text.
+        :return:
+            tree.Tree - NLTK chunks the tagged tokens into named entities based on the IOB notation, which stands
+            for Inside-Outside-Beginning.
+            This notation indicates whether a token is inside a named entity (I), outside a named entity (O), or at the
+            beginning of a named entity (B).
+            You can find more information about the IOB notation clicking
+            the link https://en.wikipedia.org/wiki/Inside%E2%80%93outside%E2%80%93beginning_(tagging).
+        """
         pos_tag_tokens = nltk.word_tokenize(self.text_removed_from_rare_words)
         tags = nltk.pos_tag(pos_tag_tokens)
         chunks = nltk.ne_chunk(tags)
         return chunks
 
-    def sentiment_analysis(self):
+    def sentiment_analysis(self) -> dict:
+        """
+        Give a sentiment intensity score to sentences.
+        :return:
+            Dict - return a float for sentiment strength based on the input text.
+            Positive values are positive valence, negative value are negative valence.
+            Note:
+                Hashtags are not taken into consideration (e.g. #BAD is neutral).
+                If you are interested in processing the text in the hashtags too, then
+                we recommend preprocessing your data to remove the #, after
+                which the hashtag text may be matched as if it was a normal word in the sentence.
+        """
         sia = SentimentIntensityAnalyzer()
         scores = sia.polarity_scores(self.text_removed_from_rare_words)
         return scores
