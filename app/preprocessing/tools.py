95773524deb56ee904c62d3d0e65a54548abeef0
annalitvin <litvin_any@ukr.net>
2024-07-17 07:53:28 +0300

    changed config params

--- app/preprocessing/tools.py
+++ app/preprocessing/tools.py
@@ -2,7 +2,7 @@ import pickle
 from os.path import join
 from nltk.corpus import stopwords as nltk_stopwords
 
-from app.core.config import settings
+from app.preprocessing.config import config
 
 
 def serialize(data, file_path):
@@ -13,7 +13,7 @@ def serialize(data, file_path):
 def upload_stopwords():
     """Load stopwords from all the sources we got"""
     extra_stopwords = set()
-    stopwords_file = join(settings.RAW_STOPWORDS_FILE_PATH, "stopwords.txt")
+    stopwords_file = join(config.raw_stopwords_file_path, "stopwords.txt")
     with open(stopwords_file, "r") as f:
         words = f.read()
         stop_words = [word.strip() for word in words.split(",")]
@@ -22,14 +22,14 @@ def upload_stopwords():
     stopwords = set()
     stopwords.update(set(nltk_stopwords.words("english")))
     stopwords.update(extra_stopwords)
-    stopwords.update(settings.CUSTOM_STOPWORDS)
+    stopwords.update(config.custom_stopwords)
     stopwords_list = list(stopwords)
 
-    stopwords_dump_file = join(settings.STOPWORDS_FILE_PATH, "stopwords.pkl")
+    stopwords_dump_file = join(config.stopwords_file_path, "stopwords.pkl")
     serialize(stopwords_list, stopwords_dump_file)
     return stopwords
 
 
 def get_stopwords():
-    with open(join(settings.STOPWORDS_FILE_PATH, "stopwords.pkl"), "rb") as f:
+    with open(join(config.stopwords_file_path, "stopwords.pkl"), "rb") as f:
         return pickle.load(f)
